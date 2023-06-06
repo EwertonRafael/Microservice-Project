452db326da3668d200252ee614ea4f6d888df9fa
Ewerton Rafael <ton_rafael@hotmail.com>
2023-06-06 01:10:34 -0300

    loadBalancer

--- hr-payroll/src/main/java/com/example/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/payroll/services/PaymentService.java
@@ -1,12 +1,7 @@
 package com.example.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.example.payroll.entities.Payment;
 import com.example.payroll.entities.Worker;
